6511393e2bb2c92896b543eca1d67f7c98182443
lcosgrave <33831831+lcosgrave@users.noreply.github.com>
2017-11-21 16:53:20 +0000

    fixed sending authentication response

--- eventPage.js
+++ eventPage.js
@@ -1,12 +1,46 @@
-let apiKey;
+let gotAuthKey;
+let gotUserId;
+let gotInstallationId;
+let gotClientVersion;
+let gotInstallationDomain;
 
-let chatWebSocket = new WebSocket("wss://chat-socks.teamwork.com");
-chatWebSocket.onopen = function(event){
-    console.log(event.data);
+
+    let chatWebSocket = new WebSocket("wss://chat-socks.teamwork.com");
+     chatWebSocket.onmessage = function (event) {
+         var msg = JSON.parse(event.data);
+         console.log(msg);
+
+
+
+
+
+
+
+
+
+
+
 }
-chatWebSocket.onmessage = function (event) {
-  console.log(event.data);
+sendAuthenticationResponse = function () {
+    var authenticationResponse = {
+                             source: {
+                             name: 'Teamwork Chat - Chrome Extension',
+                             version: '1.0.0'
+                             },
+                             contents: {
+                             authKey: gotAuthKey,
+                             userId:  gotUserId,
+                             installationId: gotInstallationId,
+                             clientVersion: '1.0.0',
+                             installationDomain: gotInstallationDomain,
+                             status: null
+                             }
+                               }
+    chatWebSocket.send(authenticationResponse);
+    console.log(authenticationResponse);
+
 }
+
 function getUserProfile(){
     return axios.get('https://digitalcrew.teamwork.com/chat/v3/me?includeAuth=true')
     .then(function (response) {
@@ -27,7 +61,14 @@ chrome.runtime.onInstalled.addListener(function() {
         return userProfile;
     })
     .then(function(userProfile)  {
-        apiKey = userProfile.account.user.apiKey;
+        gotAuthKey = userProfile.account.authkey;
+        gotUserId = userProfile.account.user.id;
+        gotInstallationId = userProfile.account.installationId;
+        gotInstallationDomain = userProfile.account.url;
+    
+    })
+    .then(function(){
+        sendAuthenticationResponse();
     })
     .catch((error) => {
         console.error(error);
@@ -45,6 +86,7 @@ chrome.browserAction.onClicked.addListener(function() {
             updateBadgeCount("?");
         });
 });
+
 
 
 
