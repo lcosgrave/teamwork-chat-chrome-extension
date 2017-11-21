5ade7b4762b20e432dba81b5b8fb957129499f84
lcosgrave <33831831+lcosgrave@users.noreply.github.com>
2017-11-21 09:33:24 +0000

    Added axios.min.js to manifest and removed socket.io and axios from res

--- eventPage.js
+++ eventPage.js
@@ -1,12 +1,15 @@
+}
 function setMessageCount () {
 var messageCount = "00000";
 chrome.browserAction.setBadgeText({text: messageCount});
 }
 
 
+
 chrome.runtime.onInstalled.addListener(function() {
   console.log('setting Message Count');
  setMessageCount();
+
 });
 
 
