6d1eed8cb7895393793e65b518a3141e23c753eb
lcosgrave <33831831+lcosgrave@users.noreply.github.com>
2017-11-21 15:19:31 +0000

    Added web socket

--- eventPage.js
+++ eventPage.js
@@ -1,15 +1,49 @@
+let apiKey;
+
+let chatWebSocket = new WebSocket("wss://chat-socks.teamwork.com");
+chatWebSocket.onopen = function(event){
+    console.log(event.data);
 }
-function setMessageCount () {
-var messageCount = "00000";
-chrome.browserAction.setBadgeText({text: messageCount});
+chatWebSocket.onmessage = function (event) {
+  console.log(event.data);
+}
+function getUserProfile(){
+    return axios.get('https://digitalcrew.teamwork.com/chat/v3/me?includeAuth=true')
+    .then(function (response) {
+        return response.data
+    });
 }
 
-
+function updateBadgeCount(importantUnread){
+    chrome.browserAction.setBadgeText({
+        text: importantUnread.toString()
+    });
+}
 
 chrome.runtime.onInstalled.addListener(function() {
-  console.log('setting Message Count');
- setMessageCount();
+    getUserProfile()
+    .then(function(userProfile)  {
+        updateBadgeCount(userProfile.account.counts.importantUnread);
+        return userProfile;
+    })
+    .then(function(userProfile)  {
+        apiKey = userProfile.account.user.apiKey;
+    })
+    .catch((error) => {
+        console.error(error);
+        updateBadgeCount("?");
+    });
+});
 
+chrome.browserAction.onClicked.addListener(function() {
+    getUserProfile()
+    .then((userProfile) => {
+            updateBadgeCount(userProfile.account.counts.importantUnread);
+        })
+    .catch((error) => {
+            console.error(error);
+            updateBadgeCount("?");
+        });
 });
 
 
